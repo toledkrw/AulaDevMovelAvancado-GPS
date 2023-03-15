aeb8829b44804b412cc46555ec6c42fa35834d0a
Matheus Toledo <57298489+toledkrw@users.noreply.github.com>
2023-03-15 22:56:38 +0000

    created async storage class

---
+++ projeto/database/Storage.js
@@ -0,0 +1,25 @@
+import AsyncStorage from '@react-native-community/async-storage';
+
+export default class Storage {
+    
+    async save(key, value) {
+        await AsyncStorage.setItem(key.toString(), JSON.stringify(value));
+    }
+
+    async listContent(){
+        const keys = await AsyncStorage.getAllKeys();
+        const result = await AsyncStorage.multiGet(keys);
+
+        return result
+    }
+
+    async clearContent(){
+        const keys = await AsyncStorage.getAllKeys();
+        await AsyncStorage.multiRemove(keys);
+    }
+
+    async load(key) {
+        const value = await AsyncStorage.getItem(key.toString());
+        return JSON.parse(value);
+    }
+}
